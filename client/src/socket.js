02f818d87c418af34cbd7efb2e1ed5de189d0b47
gpchandrarao <128778603+Gpchandrarao@users.noreply.github.com>
2024-03-28 11:38:49 +0530

    updates

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const socketUrl =
-  import.meta.env.MODE === "development" ? "http://localhost:4412" : "/";
+  import.meta.env.MODE === "development"
+    ? "https://cuemath-backend-px4o.onrender.com/"
+    : "/";
 
 const socket = io(socketUrl);
 export default socket;
